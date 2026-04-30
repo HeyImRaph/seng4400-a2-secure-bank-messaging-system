ef5ec10da47bc646f82dd0b83865fd4c77984fb9
Raph <l.galang2912@gmail.com>
2026-04-30 14:39:52 +1000

    Update lambda_function.py

--- backend/replyMessage/lambda_function.py
+++ backend/replyMessage/lambda_function.py
@@ -20,7 +20,7 @@ def lambda_handler(event, context):
 
         if not thread_id or not staff_name or not message_body:
             return response(400, {
-                "ERROR": "threadId, staffName, and messageBody are required"
+                "ERROr": "threadId, staffName, and messageBody are required"
             })
 
         thread_result = table.scan(
